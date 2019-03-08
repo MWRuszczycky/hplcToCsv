1f6007cd0b8d2e1b243456213878faf9c0c06fd6
MWRuszczycky <mwruszczycky@gmail.com>
2019-03-08 13:10:55 -0600

    Fixes tests to work with a test directory
    A new test directory is created and populated with the test files
    prior to testing. If the test directory already exist before running
    the tests, then it is deleted first. After running the tests, the
    entire directory is deleted.

--- tests/BasicTests.hs
+++ tests/BasicTests.hs
@@ -2,17 +2,19 @@
 -- Do not add a module declaration or it will fail to compile
 
 import qualified Controller as C
-import Control.Exception            ( bracket_         )
-import Control.Monad                ( join             )
+import Control.Exception            ( bracket_                  )
+import Control.Monad                ( join                      )
 import System.Directory             ( copyFile
-                                    , listDirectory
-                                    , removeFile       )
+                                    , createDirectory
+                                    , doesDirectoryExist
+                                    , removeDirectoryRecursive
+                                    , listDirectory             )
 import Test.Hspec                   ( Spec (..)
                                     , around_
                                     , describe
                                     , hspec
                                     , it
-                                    , shouldBe         )
+                                    , shouldBe                  )
 
 main :: IO ()
 main = hspec $ around_ manageIOTests $ do
@@ -26,12 +28,15 @@ mockConversion = it "basic conversion test" action
                       expected <- readFile "tests/files/Beckman1.dat.csv"
                       result `shouldBe` expected
 
-
 manageIOTests :: IO () -> IO ()
-manageIOTests = bracket_ setupFiles tearDown
-    where srcDir     = "tests/files/"
-          testDir    = "tests/testing/"
-          tearDown   = join $ mapM_ ( removeFile . (testDir ++ ) )
-                       <$> listDirectory testDir
-          setupFiles = copyFile ( srcDir  ++ "Beckman1.dat.asc" )
-                                ( testDir ++ "Beckman1.dat.asc" )
+manageIOTests = bracket_ setup tearDown
+    where srcDir   = "tests/files/"
+          testDir  = "tests/testing/"
+          setup    = do tearDown
+                        createDirectory testDir
+                        copyFile ( srcDir  ++ "Beckman1.dat.asc" )
+                                 ( testDir ++ "Beckman1.dat.asc" )
+          tearDown = do exists <- doesDirectoryExist testDir
+                        if exists
+                           then removeDirectoryRecursive testDir
+                           else pure ()
