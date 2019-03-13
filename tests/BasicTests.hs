8b58b049e4a1853cf7e4eb580bdd7b53335f424b
MWRuszczycky <mwruszczycky@gmail.com>
2019-03-13 18:43:37 -0500

    Refactors tests

--- tests/BasicTests.hs
+++ tests/BasicTests.hs
@@ -2,8 +2,9 @@
 -- Do not add a module declaration or it will fail to compile
 
 import qualified Controller as C
+import qualified Model      as M
 import Control.Exception            ( bracket_                  )
-import Control.Monad                ( join                      )
+import Control.Monad                ( when                      )
 import System.Directory             ( copyFile
                                     , createDirectory
                                     , doesDirectoryExist
@@ -19,24 +20,29 @@ import Test.Hspec                   ( Spec (..)
 main :: IO ()
 main = hspec $ around_ manageIOTests $ do
     describe "hplcToCsv converts Beckman output to csv" $
-        mockConversion
+        mockConversion "Beckman1.dat.asc" "Beckman1.dat.csv"
 
-mockConversion :: Spec
-mockConversion = it "basic conversion test" action
-    where action = do C.routeArg "tests/testing/Beckman1.dat.asc"
-                      result   <- readFile "tests/testing/Beckman1.dat.csv"
-                      expected <- readFile "tests/files/Beckman1.dat.csv"
+srcDirectory, testDirectory :: FilePath
+srcDirectory  = "tests/files/"
+testDirectory = "tests/testing/"
+
+mockConversion :: FilePath -> FilePath -> Spec
+mockConversion source target = it "basic conversion test" action
+    where action = do fp <- setupSrcFile source
+                      C.routeArg fp
+                      result   <- readFile . M.changeName $ fp
+                      expected <- readFile ( srcDirectory ++ target )
                       result `shouldBe` expected
+
+setupSrcFile :: FilePath -> IO FilePath
+setupSrcFile name = do
+    let fromPath = srcDirectory  ++ name
+        toPath   = testDirectory ++ name
+    copyFile fromPath toPath
+    pure toPath
 
 manageIOTests :: IO () -> IO ()
 manageIOTests = bracket_ setup tearDown
-    where srcDir   = "tests/files/"
-          testDir  = "tests/testing/"
-          setup    = do tearDown
-                        createDirectory testDir
-                        copyFile ( srcDir  ++ "Beckman1.dat.asc" )
-                                 ( testDir ++ "Beckman1.dat.asc" )
-          tearDown = do exists <- doesDirectoryExist testDir
-                        if exists
-                           then removeDirectoryRecursive testDir
-                           else pure ()
+    where setup    = tearDown >> createDirectory testDirectory
+          tearDown = do exists <- doesDirectoryExist testDirectory
+                        when exists $ removeDirectoryRecursive testDirectory
